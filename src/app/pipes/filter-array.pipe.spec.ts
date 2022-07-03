a646b640a9a4c81052cd4afef4f3aaa170abeb24
Itay Zonshine <ezonsh@email.com>
2022-07-03 23:50:30 +0300

    Auto-generated commit [ci skip]

--- src/app/pipes/filter-array.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { filterArrayPipe } from './filter-array.pipe';
-
-describe('filterArrayPipe', () => {
-  it('create an instance', () => {
-    const pipe = new filterArrayPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
