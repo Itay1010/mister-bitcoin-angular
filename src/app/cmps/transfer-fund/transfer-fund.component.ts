964cdd79f4ab0dbc210cee6827363134c37c8c2c
Itay Zonshine <ezonsh@gmail.com>
2022-07-04 12:52:25 +0300

    fixed - bug in transfer-fund cmp

--- src/app/cmps/transfer-fund/transfer-fund.component.ts
+++ src/app/cmps/transfer-fund/transfer-fund.component.ts
@@ -1,5 +1,5 @@
-import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
-import { FormControl, FormControlOptions, NgForm, ValidatorFn } from '@angular/forms';
+import { Component, Input, OnInit } from '@angular/core';
+import { NgForm, } from '@angular/forms';
 import { Contact } from 'src/app/modules/main.module';
 import { UserService } from 'src/app/services/user.service';
 
@@ -23,11 +23,11 @@ export class TransferFundComponent implements OnInit {
   async onTransferCoins(form: NgForm) {
     const { amount } = form.value
     if (!this.contact?._id || amount <= 0) return alert('value cannot be 0')
-    form.reset({ amount: 0 })
     this.isSubmitting = true
     const { name, _id } = this.contact
     await this.userService.transferCoin(-this.amount, { name, _id })
     this.isSubmitting = false
+    this.amount = 0
   }
 
 }
